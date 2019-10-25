3aaf5a9b86294346d4325b3d61684abe738f67d6
subinio <subinjin22@gmail.com>
2019-10-25 20:10:31 +0900

    first lec practice

--- 1. 숫자형, 문자열, 입출력.py
+++ 1. 숫자형, 문자열, 입출력.py
@@ -36,3 +36,18 @@ print(a + b + c)
 a = input()
 
 print("Hello, "+a+"!")
+
+
+
+
+## N students take K apples and distribute them among each other evenly.
+## The remaining (the indivisible) part remains in the basket.
+## How many apples will each single student get?
+## How many apples will remain in the basket?
+
+n = int(input())
+k = int(input())
+
+print(k // n)
+print(k % n)
+
