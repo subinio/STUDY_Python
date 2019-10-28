145ddd7858635c878a13ccf5e2e754a0d069bad8
subinio <subinjin22@gmail.com>
2019-10-28 11:19:04 +0900

    first lec practice

--- 1. 숫자형, 문자열, 입출력.py
+++ 1. 숫자형, 문자열, 입출력.py
@@ -86,3 +86,20 @@ print(second2-second1)
 
 
 
+
+
+## Given a two-digit integer, print its left digit(a tens digit) and then its right digit(a ones digit).
+## Use the operator of integer division for obtaining the tens digit and the operator of taking remainder for obtaining the ones digit.
+
+n = int(input())
+
+print(n//10, n%10)
+
+
+
+
+
+## 
+
+
+
