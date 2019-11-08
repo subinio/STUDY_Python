d9b00fc1f49d1f40d588b2e92578e1689639c777
subinio <subinjin22@gmail.com>
2019-11-08 17:44:13 +0900

    python lec2 practice

---
+++ 2. 조건문.py
@@ -0,0 +1,27 @@
+## Given an integer, print "odd" if it's odd and print "even" otherwise.
+
+a = int(input())
+
+if a%2 ==1:
+  print("odd")
+else:
+  print("even")
+
+
+
+
+
+## Given the two integers, print the least of them.
+
+a = int(input())
+b = int(input())
+
+if a>b:
+  print(b)
+else:
+  print(a)
+
+
+
+
+
