021a4acbb70af94e82beb4bef5049d4ce89d4b9f
subinio <subinjin22@gmail.com>
2019-11-10 23:53:31 +0900

    python lec2 practice

--- 2. 조건문.py
+++ 2. 조건문.py
@@ -37,3 +37,22 @@ elif X==0:
   
 else:
   print(1)
+
+
+
+
+
+## Given an integer, print "YES" if it's a three-digit number and print "NO" otherwise.
+
+a = int(input())
+
+if a>=100 and a<1000:
+  print("YES")
+else:
+  print("NO")
+
+
+
+
+
+## 
