4afe14e61df5869f65ef5a32611e9b372995d8ba
subinio <subinjin22@gmail.com>
2019-11-10 23:52:40 +0900

    python lec2 practice

--- 2. 조건문.py
+++ 2. 조건문.py
@@ -24,4 +24,16 @@ else:
 
 
 
+## For the given integer X print 1 if it's positive, -1 if it's negative, or 0 if it's equal to zero.
+## Try to use the cascade if-elif-else for it.
 
+X = int(input())
+
+if X<0:
+  print(-1)
+  
+elif X==0:
+  print(0)
+  
+else:
+  print(1)
