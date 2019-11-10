e87edf267db69e4e70fb25de22a34279cdb8a8af
subinio <subinjin22@gmail.com>
2019-11-10 23:54:02 +0900

    python lec2 practice

--- 2. 조건문.py
+++ 2. 조건문.py
@@ -55,4 +55,15 @@ else:
 
 
 
-## 
+## Given two non-zero integers, print "YES" if exactly one of them is positive and print "NO" otherwise.
+
+a = int(input())
+b = int(input())
+
+if (a>0 and b<0) or (a<0 and b>0):
+  print("YES")
+
+else:
+  print("NO")
+
+  
