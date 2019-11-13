47ddad419743c4f1c3b98f3bbf85d66a8ebd13a3
subinio <subinjin22@gmail.com>
2019-11-13 23:58:24 +0900

    python lec2 practice num11

--- 2. 조건문.py
+++ 2. 조건문.py
@@ -169,4 +169,35 @@ else:
 
 
 
+
+
+## Given three integers. Determine how many of them are equal to each other.
+## The program must print one of the numbers: 3 (if all are same), 2 (if two of them are equal to each other and the third one is different) or 0 (if all numbers are different).
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+cnt=0
+
+if a==b:
+  cnt=2
+  if a==c:
+    cnt=3
+    
+if b==c:
+  cnt=2
+  if b==a:
+    cnt=3
+  
+if c==a:
+  cnt=2
+  if c==b:
+    cnt=3
+  
+print(cnt)
+
+
+
+
   
