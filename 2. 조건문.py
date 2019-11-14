d0874a6007054e29effd4b4844bf682e5d856621
subinio <subinjin22@gmail.com>
2019-11-14 17:29:28 +0900

    python lec2 practice num12

--- 2. 조건문.py
+++ 2. 조건문.py
@@ -201,3 +201,22 @@ print(cnt)
 
 
   
+## Given three integers, in which two are equal to each other and the third one is different.
+## Print the order number of this different one - 1, 2 or 3.
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a==b:
+  print(3)
+  
+if a==c:
+  print(2)
+
+if b==c:
+  print(1)
+
+
+
+
