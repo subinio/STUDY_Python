71b8334934b8c6fc6c7af3f9bdde095aef501b8c
subinio <subinjin22@gmail.com>
2019-11-15 17:45:12 +0900

    python lec2 practice

--- 2. 조건문.py
+++ 2. 조건문.py
@@ -220,3 +220,21 @@ if b==c:
 
 
 
+
+## Given the year number. You need to check if this year is a leap year. If it is, print LEAP, otherwise print COMMON.
+## a year is a leap year if its number is exactly divisible by 4 and is not exactly divisible by 100
+## a year is always a leap year if its number is exactly divisible by 400
+
+a = int(input())
+
+if (a%4 == 0 and a%100 != 0) or (a%400 == 0):
+  print ("LEAP")
+
+else:
+  print("COMMON")
+  
+
+
+
+
+## 14
